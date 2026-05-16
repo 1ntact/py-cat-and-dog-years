1f649cf6b2d87f41487f965fe8040452a622f05f
1ntact <verbaivan9@gmail.com>
2026-05-16 14:07:34 +0200

    Done! (changed)

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,19 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age_when_values_under_15() -> None:
-    animal_ages = get_human_age(14, 14)
-    assert animal_ages == [0, 0]
-
-
-def test_get_human_age_two_values_15() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_get_human_age_values_24() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_get_human_age_values_28() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_get_human_age_values_100() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_result",
+    [
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age(cat_age: int,
+                       dog_age: int,
+                       expected_result: list
+                       ) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_result
